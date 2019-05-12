ccd5996eb102f140f9d7abd7ce3cdbb4ab39ed2e
Curtis <C-Rodg@users.noreply.github.com>
2019-05-11 18:42:57 -0700

    Setup inject styles in excellent way for determining font sizes

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,30 +1,73 @@
 // Libs
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 // Comps
 import Header from './Header';
 import Meta from './Meta';
 
+// Theme
+const theme = {
+	red: '#FF0000',
+	black: '#393939',
+	grey: '#3a3a3a',
+	lightGrey: '#e1e1e1',
+	offWhite: '#ededed',
+	maxWidth: '1000px',
+	boxShadow: '0 12px 24px 0 rgba(0,0,0,0.09)'
+};
+
 // Styles
 const StyledPage = styled.div`
 	background: #fff;
-	color: #000;
+	color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-	max-width: 1000px;
+	max-width: ${props => props.theme.maxWidth};
 	margin: 0 auto;
 	padding: 2rem;
+`;
+
+injectGlobal`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+    html {
+        box-sizing: border-box;
+        font-size: 10px;
+    }
+
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem;
+        line-height: 2;
+        font-family: 'radnika_next';
+    }
+
+    a {
+        text-decoration: none;
+        color: ${theme.black};
+    }
 `;
 
 class Page extends Component {
 	render() {
 		return (
-			<StyledPage>
-				<Meta />
-				<Header />
-				<Inner>{this.props.children}</Inner>
-			</StyledPage>
+			<ThemeProvider theme={theme}>
+				<StyledPage>
+					<Meta />
+					<Header />
+					<Inner>{this.props.children}</Inner>
+				</StyledPage>
+			</ThemeProvider>
 		);
 	}
 }
