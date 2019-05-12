a02f80757e827ea1b1afa4ca396235fc466876b4
Curtis <C-Rodg@users.noreply.github.com>
2019-05-11 18:21:45 -0700

    Added styling for main page layout

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,18 +1,30 @@
 // Libs
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
 // Comps
 import Header from './Header';
 import Meta from './Meta';
 
+// Styles
+const StyledPage = styled.div`
+	background: #fff;
+	color: #000;
+`;
+
+const Inner = styled.div`
+	max-width: 1000px;
+	margin: 0 auto;
+	padding: 2rem;
+`;
+
 class Page extends Component {
 	render() {
 		return (
-			<div>
+			<StyledPage>
 				<Meta />
 				<Header />
-				{this.props.children}
-			</div>
+				<Inner>{this.props.children}</Inner>
+			</StyledPage>
 		);
 	}
 }
