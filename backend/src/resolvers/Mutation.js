c7f876eeb53b2be0e78fc34599ff31a0ad7771de
Curtis <C-Rodg@users.noreply.github.com>
2019-05-15 21:23:54 -0700

    Fix for prisma update ID issues

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,15 @@
 const Mutations = {
-	createDog(parent, args, ctx, info) {
-		// TODO:
+	async createItem(parent, args, ctx, info) {
+		// TODO: Check if they are logged in
+
+		const item = await ctx.db.mutation.createItem(
+			{
+				...args
+			},
+			info
+		);
+
+		return item;
 	}
 };
 
