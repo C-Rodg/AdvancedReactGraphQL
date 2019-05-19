96097b7f362fc9b2376e60db1041818276f72e9e
Curtis <C-Rodg@users.noreply.github.com>
2019-05-19 16:26:16 -0700

    complete pagination logic - no cache invalidation yet

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,13 +4,16 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 
+// Settings
+import { perPage } from '../config';
+
 // Components
 import Item from './Item';
 import Pagination from './Pagination';
 
 export const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
+	query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+		items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
 			id
 			title
 			price
@@ -38,7 +41,15 @@ class Items extends Component {
 		return (
 			<Center>
 				<Pagination page={this.props.page} />
-				<Query query={ALL_ITEMS_QUERY}>
+				<Query
+					query={ALL_ITEMS_QUERY}
+					//fetchPolicy="network-only"
+
+					variables={{
+						skip: this.props.page * perPage - perPage,
+						first: perPage
+					}}
+				>
 					{({ data, error, loading }) => {
 						if (loading) return <p>Loading...</p>;
 						if (error) return <p>Error: {error.message}</p>;
